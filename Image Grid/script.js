b713254f4e7da89a679c4b8b6e944180355ed40a
AbhishekShrivastav73 <abhishekshrivastav2121@gmail.com>
2024-03-26 18:23:45 +0530

    image grid completed

---
+++ Image Grid/script.js
@@ -0,0 +1,11 @@
+let imgContainer = document.querySelector('.imgContainer');
+
+function oneColumns(){
+    imgContainer.style.columns = 1;
+}
+function twoColumns(){
+    imgContainer.style.columns = 2;
+}
+function fourColumns(){
+    imgContainer.style.columns = 4;
+}
