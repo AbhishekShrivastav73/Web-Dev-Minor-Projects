d4657eb23c8171a9f47e69263c11bec915df06ae
AbhishekShrivastav73 <abhishekshrivastav2121@gmail.com>
2024-03-26 17:49:22 +0530

    Vertical Tabs

---
+++ Vertical Tabs/Script.js
@@ -0,0 +1,16 @@
+let buttons = document.querySelectorAll('button');
+let tabs = document.querySelectorAll('.tab-pane');
+
+console.log(buttons);
+console.log(tabs);
+
+for (let index = 0; index < buttons.length; index++) {
+    buttons[index].addEventListener('click', function(){
+        console.log(this);
+        for (let i = 0; i < buttons.length; i++) {
+            tabs[i].classList.remove('active');
+        }
+        tabs[index].classList.add('active');
+    })
+    
+}   
