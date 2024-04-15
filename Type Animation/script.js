cb67fd0cab90dd0e7720b252dbd143e3611c67fb
AbhishekShrivastav73 <abhishekshrivastav2121@gmail.com>
2024-04-15 23:49:17 +0530

    fixed committed

--- Type Animation/script.js
+++ Type Animation/script.js
@@ -1,12 +1,26 @@
-let text = 'Hey Im a Front-End Web Developer';
+let text = [
+    { content :  'Abhishek'},
+    { content :  'a Front-End Web Developer'},
+    { content :  'a back-end Developer'},
+];
 let i = 0;
 let element = document.querySelector('#text');
+let a = 0;
 
-function animation(){
-    if (i<text.length){
-        element.innerHTML += text.charAt(i);
-        i++;
+function animation() {
+    if (i < text.length) {
+        if (a < text[i].content.length) {
+            if (a === 0) {
+                element.innerHTML = '';
+            }
+            element.innerHTML += text[i].content.charAt(a);
+            a++;
+        } else {
+            i++;
+            a = 0;
+        }
         setTimeout(animation, 100);
     }
 }
 animation();
+// setInterval(animation(), 40);
